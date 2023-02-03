1e1b0bc6ec5c7b1621eebf8008e779a3875de9fc
Re <hikarisakata91@gmail.com>
2023-02-03 19:57:17 +0800

    lol :v

--- src/components/molecules/BigCardGames.jsx
+++ src/components/molecules/BigCardGames.jsx
@@ -5,18 +5,17 @@ const BigCardGames = () => {
         <>  
             <div className="px-5 ">
               <div className="rounded-xl h-[30rem] bg-cover bg-center bg-gradient-black" style={{backgroundImage: `url(/images/games/dishonored.webp)`}}>
-                {/* <img src="/images/games/dishonored.webp" className="" alt="" /> */}
-                <div className="py-60 pl-3 pr-7 ">
+                <div className="absolute inset-0 bg-gradient-to-t md:bg-gradient-to-r from-black/80 md:from-black/40 to-transparent"></div>
+                <div className="absolute bottom-14 px-5 left-5 md:left-8 text-sm md:font-bold w-[80%] md:w-[40%]">
                   <div>
                     <h2>OUT NOW</h2>
                     <p>Dishonored is an immersive first-person action game that casts you as a supernatural assasin driven by revenge.</p>
                   </div>
-                  <div>
+                  <div className="mt-7">
                     <p>Starting at</p>
                     <p>IDR 200.500</p>
                   </div>
                 </div>
-                
               </div>
             </div>
         </>
--- src/components/molecules/Search.jsx
+++ src/components/molecules/Search.jsx
@@ -16,23 +16,25 @@ const Search = () => {
 
     return (
         <>
-            <div className="bg-[#121212] text-white border-b border-[#343434] pt-4 pb-7  flex justify-center items-center text-sm font-bold w-full text-center top-14">
-                <div className="" onClick={openFilter}>
-                    <p className="text-[10px] text-[#868686]">EPIC GAMES STORE</p>
-                    <p className="">Discover</p>
+            <div className="bg-[#121212] bg-opacity-[0.85]">
+                <div className="backdrop-blur-xl text-white border-b border-[#343434] pt-4 pb-7  flex justify-center items-center text-sm font-bold w-full text-center top-14">
+                    <div className="" onClick={openFilter}>
+                        <p className="text-[10px] text-[#868686]">EPIC GAMES STORE</p>
+                        <p className="">Discover</p>
+                    </div>
+                    <div className="absolute right-5">
+                        <SearchIcon />
+                    </div>
                 </div>
-                <div className="absolute right-5">
-                    <SearchIcon />
+                <div className={`bg-[#121212] absolute w-full top-[8.8rem] z-20 ${open ? "block"  : "hidden" }`}>
+                    <ul className="text-center text-white py-10">
+                        {
+                            links.map( link => (
+                                    <li className="py-7" key={link.id}>{link.title}</li>
+                            ))
+                        }
+                    </ul>
                 </div>
-            </div>
-            <div className={`bg-[#121212] absolute w-full top-[8.8rem] z-20 ${open ? "block"  : "hidden" }`}>
-                <ul className="text-center text-white py-10">
-                    {
-                        links.map( link => (
-                                <li className="py-7" key={link.id}>{link.title}</li>
-                        ))
-                    }
-                </ul>
             </div>
         </>
     )
