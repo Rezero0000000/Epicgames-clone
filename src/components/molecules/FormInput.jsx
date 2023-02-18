5ae07d36d6b98cfa7e55f39946771468e973377d
Re <hikarisakata91@gmail.com>
2023-02-18 15:29:01 +0800

    >'

---
+++ src/components/molecules/FormInput.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const FormInput = (porps) => {
+    const {title} = porps;
+
+    return (
+        <> 
+        <div className="relative w-full p-1 px-5 border mt-7 rounded-md border-white/30 z-20">
+        <button id="headlessui-listbox-button-13" type="button" aria-haspopup="true" aria-expanded="false" className="relative flex items-center justify-between w-full gap-2 py-3 text-sm group">
+            <div className="text-left">
+                <div className="text-white/60">Country <span className="text-red-400">*</span></div>
+                <div>{title}</div>
+            </div>
+        </button>
+         </div>
+        </>
+    )
+}
+
+export default FormInput;
--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,20 +1,57 @@
 import React from "react";
+import FormInput from "../components/molecules/FormInput";
 
 const Login = () => {
     return (
         <>
-            <div className="bg-[#202020] px-5 mt-10 text-white py-14">
+            <div className="bg-[#202020] w-full sm:w-[30.5rem] mx-auto px-12 mt-10 text-white py-14">
                 <div>
                     <img src="/images/eg-logo-light.svg" className="w-10 mx-auto" alt="" />
-                    <p className="text-sm font-semibold mt-7">SIGN IN WITH AN EPIC GAMES ACCOUNT</p>
+                    <p className="text-sm font-semibold mt-7">SIGN UP</p>
                 </div>
                 <div className="mt-5">
                     <form action="w-full ">
+                         <div className="relative w-full p-1 px-5 border rounded-md border-white/30 z-20">
+                            <button id="headlessui-listbox-button-13" type="button" aria-haspopup="true" aria-expanded="false" className="relative flex items-center justify-between w-full gap-2 py-3 text-sm group">
+                                <div className="text-left">
+                                    <div className="text-white/60">Country <span className="text-red-400">*</span></div>
+                                    <div>indonesia</div>
+                                </div>
+                                <svg width="1em" height="1em" viewBox="0 0 24 24" className="w-4 h-4 transition duration-200">
+                                    <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6l1.41-1.41z" fill="currentColor"></path>
+                                </svg>
+                            </button>
+                        </div>
+                        <div className="flex justify-between gap-2">
+                            <FormInput title="First Name" />
+                            <FormInput title="Last Name" />
+                        </div>
                         <div>
-                         <input type="text" id="email" className="border-white/30  focus:border-white appearance-none floating-input peer"/>
-                            <label htmlFor="email" className="floating-label peer-focus:-translate-y-5 peer-focus:scale-90 -translate-y-1/2">Email Address</label>
+                            <FormInput title="Display Name" />
+                            <FormInput title="Emaill Address" />
+                            <FormInput title="Password" />
+                        </div>
+                        <div>
+                            <div className="flex items-center mt-7">
+                                <input type="checkbox" className="mr-5 grid w-8 h-8 border ring-8 ring-transparent hover:ring-white/10 place-items-center focus:outline-none"/>
+                                <label className="text-sm">I would like to receive news, surveys and special offers from Epic Games.</label>
+                            </div>
+
+                            <div className="flex items-center mt-7">
+                                <input type="checkbox" className="mr-5 grid w-5 h-5 border ring-8 ring-transparent hover:ring-white/10 place-items-center focus:outline-none"/>
+                                <label className="text-sm">I have read and agree to the terms of services.</label>
+                            </div>
+                        </div>
+                        <div className="flex justify-center">
+                             <button className="px-32 bg-[#007DFC] py-3 mt-5 rounded-lg">REGISTER NOW</button>
                         </div>
                     </form>
+                </div>
+
+                <div className="text-center mt-10 text-sm text-[#A0A0A0]">
+                    <a href="" className="text-white"> Privacy Policy</a>
+                    <p className="mt-7">Have an Epic Games account? <a href="" className="text-white">Sign In</a></p>
+                    <p>Back to <a href="/" className="text-white">Home</a></p>
                 </div>
             </div>
         </>
