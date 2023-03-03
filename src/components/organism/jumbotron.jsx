47d042e1e977c144fcdf628313f049ac808d8ade
Re <hikarisakata91@gmail.com>
2023-03-03 21:24:28 +0800

    do something

--- src/components/organism/jumbotron.jsx
+++ src/components/organism/jumbotron.jsx
@@ -10,10 +10,10 @@ import 'swiper/css';
 
 const Jumbotron = () => {
 
-
     const [dataGame, setDataGame] = useState([])
     useEffect(() => {
-        fetch("http://127.0.0.1:5173/public/data/games.json")
+        console.log('s')
+        fetch("/data/games.json")
         .then(response => {
            return response.json();
         }).then(data => {
@@ -21,12 +21,9 @@ const Jumbotron = () => {
         })
     },[]);
 
-    console.log(dataGame)
 
     return (
         <>
-
-        
             <div className="md:flex justify-center">
                 <div className="md:w-[70%] w-full ">
                     <Swiper
@@ -37,44 +34,25 @@ const Jumbotron = () => {
                         pagination={{ clickable: true }}
                         scrollbar={{ draggable: true }}
                     >
-                        <SwiperSlide><BigCardGames/></SwiperSlide>
-                        <SwiperSlide><BigCardGames/></SwiperSlide>
-                        <SwiperSlide><BigCardGames/></SwiperSlide>
-                        <SwiperSlide><BigCardGames/></SwiperSlide>
-                        <SwiperSlide><BigCardGames/></SwiperSlide>
-                    </Swiper>
+                    {
+                        dataGame.map(game =><SwiperSlide key={game.id}><BigCardGames/></SwiperSlide> )
+                    }                        
+                  </Swiper>
                 </div>
                 <div className="ml-5 hidden md:block">
-                    <div className="text-white flex items-center">
+                {
+                    dataGame.map(game => 
+                    game.id == 1 ? 
+                    <div className="text-white flex items-center " key={game.id}>
                         <img src="/images/games/genshin-impact.webp" className="w-full w-12 rounded-lg h-14 object-cover" loading="lazy" alt="" />
-                        <p className="text-[13px] ml-3 mr-5">Dishonored</p>
-                    </div>
-                    <div className="text-white flex items-center mt-5">
+                        <p className="text-[13px] ml-3 mr-5">{game.title}</p>
+                    </div> :
+                    <div className="text-white flex items-center mt-5" key={game.id}>
                         <img src="/images/games/genshin-impact.webp" className="w-full w-12 rounded-lg h-14 object-cover" loading="lazy" alt="" />
-                        <p className="text-[13px] ml-3 mr-5">Dishonored</p>
-                    </div>
-                    <div className="text-white flex items-center mt-5">
-                        <img src="/images/games/genshin-impact.webp" className="w-full w-12 rounded-lg h-14 object-cover" loading="lazy" alt="" />
-                        <p className="text-[13px] ml-3 mr-5">Dishonored</p>
-                    </div>
-                    <div className="text-white flex items-center mt-5">
-                        <img src="/images/games/genshin-impact.webp" className="w-full w-12 rounded-lg h-14 object-cover" loading="lazy" alt="" />
-                        <p className="text-[13px] ml-3 mr-5">Dishonored</p>
-                    </div>
-                    <div className="text-white flex items-center mt-5">
-                        <img src="/images/games/genshin-impact.webp" className="w-full w-12 rounded-lg h-14 object-cover" loading="lazy" alt="" />
-                        <p className="text-[13px] ml-3 mr-5">Dishonored</p>
-                    </div>
-                    <div className="text-white flex items-center mt-5">
-                        <img src="/images/games/genshin-impact.webp" className="w-full w-12 rounded-lg h-14 object-cover" loading="lazy" alt="" />
-                        {
-                                dataGame.map(data2 => {
-                                return   <p key={data2.id} className="text-[13px] ml-3 mr-5">{data2.title}</p>
-                                })
-                        }
-
-                     
-                    </div>
+                        <p className="text-[13px] ml-3 mr-5">{game.title}</p>
+                    </div> 
+                    )
+                }
                 </div>
             </div>
         </>
