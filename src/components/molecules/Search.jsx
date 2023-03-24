178d4d788bb4e392bf803b79bacc58d1d68e1684
Rekun <hikarisakata91@gmail.com>
2023-03-24 10:54:16 +0700

    .

--- src/components/molecules/Search.jsx
+++ src/components/molecules/Search.jsx
@@ -18,7 +18,7 @@ const Search = () => {
 
     return (
         <>
-            <div className="bg-[#121212] fixed top-12 w-full ">
+            <div className="bg-[#121212] fixed top-12 w-full md:py-3 ">
                 <div className="backdrop-blur-sm text-white py-7 flex lg:py-3 justify-center lg:justify-start lg:px-28 xl:px-48 items-center text-[16px] w-full text-center top-14">
                     <div className="absolute left-8 lg:static">
                         <div className="lg:absolute mt-1 lg:mt-3 lg:ml-4">
@@ -37,22 +37,21 @@ const Search = () => {
                     </div>
                     <div className="hidden lg:block lg:ml-12">
                         <ul className="text-center flex text-white ">
-                            {/* {
+                            {
                                 links.map( link => (
                                   link.id == 2 ? <li className="font-normal mr-10 text-[#858585]" key={link.id}>{link.title}</li> : <li className="font-normal mr-10" key={link.id}>{link.title}</li>
                                 ))
-                            } */}
-                            <li className="py-5 text-[#8C8C8C] border-t border-[#2A2A2A]" >Discover</li>
+                            }
                         </ul>
                     </div>
            
                 </div>
                 <div className={`bg-[#121212] absolute lg:hidden w-full top-[5rem] z-20 ${open ? "block"  : "hidden" }`}>
-                    <ul className=" pl-8 text-white py-5 xs:py-6">
+                    <ul className=" pl-8 text-white py-5 xs:py-6 md:text-[14px]">
                         {
                             links.map( link => (
-                                link.id !== 1 ? <li className="py-5 text-[#8C8C8C] border-t border-[#2A2A2A]" key={link.id}>{link.title}</li>
-                                : <li className="py-5" key={link.id}>{link.title}</li>
+                                link.id !== 1 ? <li className="py-5 md:py-3 text-[#8C8C8C] border-t border-[#2A2A2A]" key={link.id}>{link.title}</li>
+                                : <li className="py-5 md:py-3" key={link.id}>{link.title}</li>
                             ))
                         }
                     </ul>
